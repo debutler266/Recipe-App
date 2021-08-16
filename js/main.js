41520baa59ae6b2f734e97cc3ddc631664833b19
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-08-16 17:00:15 -0400

    added navbar styling for large & small screens

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 const getElement = (selector) => {
+
   const element = document.querySelector(selector)
 
   if (element) return element
   throw Error(
-    `Please double check your class names, there is no ${selector} class`
+    `Please double check your class names, no ${selector} class`
   )
 }
 
 const links = getElement('.nav-links')
 const navBtnDOM = getElement('.nav-btn')
 
+//event listener with callback function passed in
 navBtnDOM.addEventListener('click', () => {
   links.classList.toggle('show-links')
 })
 
-const date = getElement('#date')
+/*const date = getElement('#date')
 const currentYear = new Date().getFullYear()
-date.textContent = currentYear
+date.textContent = currentYear*/
